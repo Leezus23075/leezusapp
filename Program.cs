e8b6ee694ef934c21d071cdf975c0ab2f1ae9886
Leezus23075 <CarleeDilworth1998@gmail.com>
2017-04-08 07:37:35 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // set color of game 
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+
+
+            // prints game name
+
+            Console.WriteLine("magic 8-ball by Lee Dilworth");
+
+            while (true)
+            {
+                Console.Write("enter question here:");
+                String question = Console.ReadLine();
+                if (question.ToLower() == "quit");
+
+                List<String> Answers = new List<string>();
+                Answers.Add("yes");
+                Answers.Add("no");
+                Answers.Add("you should reconsider");
+                Answers.Add("LOL of Course not");
+
+                Random Ranstri = new Random();
+                int index = Ranstri.Next( Answers.Count );
+                string randomString = Answers [ index ];
+
+                Console.WriteLine(randomString);
+
+                
+                }
+            }
+        }
+    }
